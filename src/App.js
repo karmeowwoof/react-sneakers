29a69e508225fec17f961ce022b388dac6883386
karmeowwoof <karmeowwoof@gmail.com>
2022-12-22 18:59:54 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2  className="mb-30" >Корзина</h2>
+          
+          <div className="items">
+            
+          <div className="cartItem d-flex align-center mb-20">    
+            <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+            <div className="mr-20">
+              <p className="mb-5" >Мужские кроссовки Nke Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" width={20} height={20} src="/img/btn-remove.svg" alt="remove" />
+          </div>
+
+          <div className="cartItem d-flex align-center mb-20">    
+            <div style={{ backgroundImage: 'url(/img/sneakers/2.jpg)'}} className="cartItemImg"></div>
+            <div className="mr-20">
+              <p className="mb-5" >Мужские кроссовки Nke Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" width={20} height={20} src="/img/btn-remove.svg" alt="remove" />
+          </div>
+          </div>
+        <div className="cartTotalBlock">
+        <ul>
+          <li>
+            <span>Итого:</span>
+            <div></div>
+            <b> 21 498 руб.</b>
+          </li>
+          <li>
+            <span>Налог 5%</span>
+            <div></div>
+            <b>1074 руб.</b>
+          </li>
+        </ul>
+        <button>Оформить заказ</button></div>  
+        
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="headerLeft d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="logo" />
